0c22c7a79566646efcf85ae70b0b5d12fc2c1dab
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2024-05-12 20:59:25 +0900

    変数名の変更、条件分岐の統合
    - ブロック引数名を変更
    - next if で条件分岐を削減

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,68 +1,55 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
+scores = ARGV[0].split(',')
 shots = []
 frame_count = 1
-
 total_shots = 0
 
-scores.each do |s|
+scores.each do |score|
   break if frame_count == 10 && total_shots >= 3
 
   if frame_count < 10
-    if s == 'X'
-      shots << 10
-      shots << 0
-      frame_count += 1
+    if score == 'X'
+       shots << 10
+       shots << 0
+       frame_count += 1
     else
       shots << s.to_i
       frame_count += 1 if shots.size.even?
     end
-  elsif frame_count == 10
-    shots << if s == 'X'
-               10
-             else
-               s.to_i
-             end
+ elsif frame_count == 10
+    shots << (score == 'X' ? 10 : score.to_i)
     total_shots += 1
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 point = 0
 
 frames.each_with_index do |frame, index|
-  if index < 8
-    point += if frame[0] == 10
+  next point += frame.sum if frame[0] != 10 && frame.sum != 10
+
+  point += if index < 8
+             if frame[0] == 10
                if frames[index + 1][0] == 10
                  20 + frames[index + 2][0]
                else
                  10 + frames[index + 1].sum
                end
-             elsif frame.sum == 10
+             else
                10 + frames[index + 1][0]
-             else
-               frame.sum
              end
-  elsif index == 8
-    point += if frame[0] == 10
+           elsif index == 8
+             if frame[0] == 10
                10 + frames[9].sum
-             elsif frame.sum == 10
-               10 + frames[9][0]
              else
-               frame.sum
+               10 + frames[9][0]
              end
-  elsif index == 9
-    point += frame.sum
-  elsif index == 10
-    point += frame[0] if frames[9][0] == 10 || frames[9].sum == 10
-  end
+           else
+             frame.sum
+           end
 end
 
 puts point
