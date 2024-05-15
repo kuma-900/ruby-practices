d757bc98bcb8731df81eb526bef0273ee2858d1d
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2024-05-15 22:55:36 +0900

    フレームカウント方法の変更、スコア計算の簡略化
    - frame_countを削除し、shots.lengthでカウント
    - スコア計算の共通箇所を統合し、簡略化した

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,46 +3,43 @@
 
 scores = ARGV[0].split(',')
 shots = []
-frame_count = 1
 shots_in_frame = []
 
 scores.each do |score|
   score_number = score == 'X' ? 10 : score.to_i
   shots_in_frame << score_number
-  shots_in_frame << 0 if score_number == 10 && frame_count < 10 && shots_in_frame.length < 2
+  shots_in_frame << 0 if score_number == 10 && shots.length < 9 && shots_in_frame.length < 2
 
   if shots_in_frame.length == 2
     shots << shots_in_frame.dup
     shots_in_frame.clear
-    frame_count += 1
   end
 
-  shots << shots_in_frame.dup if frame_count > 10
+  shots << shots_in_frame.dup if shots.length >= 10 && shots_in_frame.length.positive?
 end
 
 point = 0
 
 shots.each_with_index do |frame, index|
-  next point += frame.sum if frame[0] != 10 && frame.sum != 10
+  point += frame.sum
+  next if (frame[0] != 10 && frame.sum != 10) || index > 8
 
   point += if index < 8
              if frame[0] == 10
                if shots[index + 1][0] == 10
-                 20 + shots[index + 2][0]
+                 10 + shots[index + 2][0]
                else
-                 10 + shots[index + 1].sum
+                 shots[index + 1].sum
                end
              else
-               10 + shots[index + 1][0]
+               shots[index + 1][0]
              end
            elsif index == 8
              if frame[0] == 10
-               10 + shots[9].sum
+               shots[9].sum
              else
-               10 + shots[9][0]
+               shots[9][0]
              end
-           else
-             frame.sum
            end
 end
 
