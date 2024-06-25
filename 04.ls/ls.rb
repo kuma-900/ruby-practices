890cca44dee3e96e476b1795045918a8049c81dc
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2024-06-25 21:54:45 +0900

    メソッドの削減と定数導入
    sort_entriesメソッドの削減
    定数 MAX_WIDTH、COLUMN_COUNT、ROW_COUNTの導入

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,22 +7,18 @@ def remove_hidden_files(entries)
   entries.reject { |entry| entry.start_with?('.') }
 end
 
-def sort_entries(entries)
-  entries.sort
-end
-
 visible_entries = remove_hidden_files(entries)
 
-sorted_entries = sort_entries(visible_entries)
+sorted_entries = visible_entries.sort
 
-max_width = sorted_entries.map(&:length).max
-row_count = (sorted_entries.size / 3.0).ceil
-column_count = 3
+MAX_WIDTH = sorted_entries.map(&:length).max
+COLUMN_COUNT = 3
+ROW_COUNT = (sorted_entries.size / COLUMN_COUNT.to_f).ceil
 
-(0...row_count).each do |row|
-  (0...column_count).each do |col|
-    index = row + col * row_count
-    print sorted_entries[index].ljust(max_width + 4) if index < sorted_entries.size
+(0...ROW_COUNT).each do |row|
+  (0...COLUMN_COUNT).each do |col|
+    index = row + col * ROW_COUNT
+    print sorted_entries[index].ljust(MAX_WIDTH + 4) if index < sorted_entries.size
   end
   puts
 end
